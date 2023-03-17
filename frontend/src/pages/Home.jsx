247f1cd63fd6e457f5cf0c9deea1c460a9caaf2c
Beldaras <jphilippe.roblot@gmail.com>
2023-03-17 09:31:54 +0100

    homepage input fixes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -17,19 +17,19 @@ export default function Home() {
       <AbbePierreCitation />
       <div className="flex flex-col items-center justify-center ">
         <div className="flex flex-col ">
-          <div className="h-[240px] w-[500px] flex items-center justify-evenly gap-4 ">
-            <form className="gap-4 bject-left-top">
+          <div className="h-[240px] w-[500px] flex items-center justify-center gap-4 ">
+            <form className="flex flex-col items-center justify-center gap-4">
               <input
                 type="text"
                 name="email"
                 placeholder="E-mail"
-                className=" w-[40%] bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className=" w-[50%] bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
               />
               <input
                 type="password"
                 name="password"
                 placeholder="Mot de passe"
-                className=" w-[40%] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className=" w-[50%] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
               />
             </form>
           </div>
